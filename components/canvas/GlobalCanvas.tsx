12604c894d4752119995eb3b2dd38f7030285fa1
sanskar-a11y <sanskarsharma923@gmail.com>
2026-07-26 07:37:29 +0530

    Make 3D cat semi-transparent (40% opacity) on inner pages, full on homepage

--- components/canvas/GlobalCanvas.tsx
+++ components/canvas/GlobalCanvas.tsx
@@ -1,15 +1,21 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import { Background } from '@/components/canvas/Background'
 import { CatModel } from '@/components/canvas/CatModel'
 import { HUD } from '@/components/ui/HUD'
 
 export function GlobalCanvas() {
+  const pathname = usePathname()
+  const isHome = pathname === '/'
+
   return (
     <>
-      <Background>
-        <CatModel />
-      </Background>
+      <div className={`transition-opacity duration-700 ${isHome ? 'opacity-100' : 'opacity-40'}`}>
+        <Background>
+          <CatModel />
+        </Background>
+      </div>
       <HUD />
     </>
   )
